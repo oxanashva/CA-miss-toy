12c8d8515d343e81afb46b6571d1d7f9060e8e38
oxanashva <oxanashav@gmail.com>
2025-09-29 19:15:08 +0300

    feat: implement EditToy component markup and styles

--- src/pages/ToyEdit.jsx
+++ src/pages/ToyEdit.jsx
@@ -1,7 +1,59 @@
+import { useEffect, useRef } from "react"
+import { useNavigate } from "react-router"
 
 
 export function ToyEdit() {
+    const elDialog = useRef(null)
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        elDialog.current.showModal()
+    }, [])
+
+    function onCloseModal() {
+        elDialog.current.close()
+        navigate('/toy')
+    }
+
     return (
-        <div>Toy Edit</div>
+        <dialog ref={elDialog} className="toy-edit">
+            <h2>Edit Toy</h2>
+            <form>
+                <div className="form-group">
+                    <label htmlFor="name">Name</label>
+                    <input
+                        id="name"
+                        name="name"
+                        type="text"
+                    />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="name">Price</label>
+                    <input
+                        id="price"
+                        name="price"
+                        type="number"
+                    />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="name">Labels</label>
+                    <select
+                        id="labels"
+                        name="labels"
+                        multiselect="true"
+                    />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="name">Is In Stock</label>
+                    <input
+                        id="instock"
+                        name="instock"
+                        type="checkbox"
+                    />
+                </div>
+                <button className="btn" type="submit">Save</button>
+            </form>
+            <button className="close" type="button" onClick={onCloseModal}>X</button>
+        </dialog>
     )
 }
